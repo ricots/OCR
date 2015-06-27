83ed8b42cd0a400bbc86f2328cf02c5d6282db2c
Zeshan Aslam <zeshan12aslam@gmail.com>
2015-06-27 15:16:06 -0400

    Added demo

--- app/src/main/java/com/aslam/zeshan/ocr/PostAPI.java
+++ app/src/main/java/com/aslam/zeshan/ocr/PostAPI.java
@@ -51,7 +51,7 @@ public class PostAPI {
                 try {
                     progressDialog.setMessage("Getting Text...");
                     progressDialog.setProgress(75);
-                    
+
                     getText();
                 } catch (IOException e) {
                     e.printStackTrace();
